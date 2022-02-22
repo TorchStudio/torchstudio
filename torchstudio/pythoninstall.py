e7d3a728f4362d703f38b489a2312978bf0d2935
Robin Lobel <divide@divideconcept.net>
2022-02-22 18:21:20 +0100

    add huggingface package

--- torchstudio/pythoninstall.py
+++ torchstudio/pythoninstall.py
@@ -12,6 +12,7 @@ if importlib.util.find_spec("conda") is None:
 
 import conda.cli.python_api as Conda
 
+# datasets(+huggingface_hub) required by hugging face hub
 # scipy required by torchvision: Caltech ImageNet SBD SVHN datasets and Inception v3 GoogLeNet models
 # pandas required by the dataset tutorial: https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
 # matplotlib-base required by torchstudio renderers
@@ -20,16 +21,16 @@ import conda.cli.python_api as Conda
 # pysoundfile required on windows by torchaudio: https://pytorch.org/audio/stable/backend.html#soundfile-backend
 if sys.platform.startswith('win'):
     if args.gpu:
-        conda_install="pytorch torchvision torchaudio cudatoolkit=11.3 scipy pandas matplotlib-base python-graphviz paramiko pysoundfile"
+        conda_install="pytorch torchvision torchaudio cudatoolkit=11.3 datasets scipy pandas matplotlib-base python-graphviz paramiko pysoundfile"
     else:
-        conda_install="pytorch torchvision torchaudio cpuonly scipy pandas matplotlib-base python-graphviz paramiko pysoundfile"
+        conda_install="pytorch torchvision torchaudio cpuonly datasets scipy pandas matplotlib-base python-graphviz paramiko pysoundfile"
 elif sys.platform.startswith('darwin'):
-    conda_install="pytorch torchvision torchaudio scipy pandas matplotlib-base python-graphviz paramiko"
+    conda_install="pytorch torchvision torchaudio datasets scipy pandas matplotlib-base python-graphviz paramiko"
 elif sys.platform.startswith('linux'):
     if args.gpu:
-        conda_install="pytorch torchvision torchaudio cudatoolkit=11.3 scipy pandas matplotlib-base python-graphviz paramiko"
+        conda_install="pytorch torchvision torchaudio cudatoolkit=11.3 datasets scipy pandas matplotlib-base python-graphviz paramiko"
     else:
-        conda_install="pytorch torchvision torchaudio cpuonly scipy pandas matplotlib-base python-graphviz paramiko"
+        conda_install="pytorch torchvision torchaudio cpuonly datasets scipy pandas matplotlib-base python-graphviz paramiko"
 else:
     print("Error: Unsupported platform.", file=sys.stderr)
     print("Windows, macOS or Linux is required.", file=sys.stderr)
@@ -39,8 +40,8 @@ print("Downloading and installing PyTorch and additional packages:")
 print(conda_install)
 print("")
 
-# channels: pytorch for pytorch torchvision torchaudio, nvidia for cudatoolkit=11.1 on Linux, conda-forge for everything else except anaconda for python-graphviz
-conda_install+=" -c pytorch -c nvidia -c conda-forge -c anaconda"
+# channels: pytorch for pytorch torchvision torchaudio, nvidia for cudatoolkit=11.1 on Linux, huggingface for datasets(+huggingface_hub), conda-forge for everything else except anaconda for python-graphviz
+conda_install+=" -c pytorch -c nvidia -c huggingface -c conda-forge -c anaconda"
 
 # https://stackoverflow.com/questions/41767340/using-conda-install-within-a-python-script
 (stdout_str, stderr_str, return_code_int) = Conda.run_command(Conda.Commands.INSTALL,conda_install.split(),stdout=sys.stdout,stderr=sys.stderr)
